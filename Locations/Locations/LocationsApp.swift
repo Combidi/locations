fd83682f9f3cddedd7e141807d4230727c341744
Peter Combee <petercombee@gmail.com>
2024-11-05 20:02:07 +0100

    Add new empty project

---
+++ Locations/Locations/LocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationsApp.swift
+//  Locations
+//
+//  Created by Peter Combee on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
