fc2405dcb2f60d2561d680863cff5b25d50dc21e
Peter Combee <petercombee@gmail.com>
2024-11-05 21:00:09 +0100

    `RemoteLocationsProvider.getLocations` delivers locations on valid locations JSON

--- Locations/LocationsTests/RemoteLocationsProviderTests.swift
+++ Locations/LocationsTests/RemoteLocationsProviderTests.swift
@@ -5,7 +5,7 @@
 @testable import Locations
 
 private struct RemoteLocationsProvider: LocationsProvider {
-    
+
     private let httpClient: HTTPClientSpy
     
     init(httpClient: HTTPClientSpy) {
@@ -14,8 +14,27 @@ private struct RemoteLocationsProvider: LocationsProvider {
     
     func getLocations() async throws -> [Location] {
         let locationsURL = URL(string: "https://raw.githubusercontent.com/abnamrocoesd/assignment-ios/main/locations.json")!
-        try httpClient.get(from: locationsURL)
-        return []
+        let data = try httpClient.get(from: locationsURL)
+        
+        struct DecodableLocations: Decodable {
+            
+            struct DecodableLocation: Decodable {
+                let name: String?
+                let lat: Double
+                let long: Double
+            }
+
+            let locations: [DecodableLocation]
+        }
+        
+        let decodableLocations = try JSONDecoder().decode(DecodableLocations.self, from: data)
+                
+        let locations: [Location] = decodableLocations.locations.compactMap {
+            guard let name = $0.name else { return nil }
+            return Location(name: name, latitude: $0.lat, longitude: $0.long)
+        }
+        
+        return locations
     }
 }
 
@@ -23,11 +42,11 @@ import XCTest
 
 final class RemoteLocationsProviderTests: XCTestCase {
     
-    func test_getLocations_getsLocationsFromUrl() async throws {
+    func test_getLocations_getsLocationsFromUrl() async {
         let client = HTTPClientSpy()
         let sut = RemoteLocationsProvider(httpClient: client)
         
-        _ = try await sut.getLocations()
+        _ = try? await sut.getLocations()
         
         let expectedUrl = URL(string: "https://raw.githubusercontent.com/abnamrocoesd/assignment-ios/main/locations.json")!
         XCTAssertEqual(client.capturedUrls, [expectedUrl])
@@ -38,7 +57,7 @@ final class RemoteLocationsProviderTests: XCTestCase {
         let sut = RemoteLocationsProvider(httpClient: client)
         
         let error = NSError(domain: "any", code: 0)
-        client.stub = error
+        client.stub = .failure(error)
         
         do {
             _ = try await sut.getLocations()
@@ -47,18 +66,67 @@ final class RemoteLocationsProviderTests: XCTestCase {
             XCTAssertEqual(capturedError as NSError, error)
         }
     }
+    
+    func test_getLocations_deliversLocationsOnValidLocationsJson() async throws {
+        let client = HTTPClientSpy()
+        let sut = RemoteLocationsProvider(httpClient: client)
+        
+        let locationsJson = """
+        {
+          "locations": 
+          [
+            {
+              "name": "Amsterdam",
+              "lat": 52.3547498,
+              "long": 4.8339215
+            },
+            {
+              "name": "Mumbai",
+              "lat": 19.0823998,
+              "long": 72.8111468
+            },
+            {
+              "name": "Copenhagen",
+              "lat": 55.6713442,
+              "long": 12.523785
+            },
+            {
+              "lat": 40.4380638,
+              "long": -3.7495758
+            }
+          ]
+        }
+        """
+        
+        let validLocationsJsonData = Data(locationsJson.utf8)
+        
+        client.stub = .success(validLocationsJsonData)
+        
+        let locations = try await sut.getLocations()
+        
+        let expectedLocations = [
+            Location(name: "Amsterdam", latitude: 52.3547498, longitude: 4.8339215),
+            Location(name: "Mumbai", latitude: 19.0823998, longitude: 72.8111468),
+            Location(name: "Copenhagen", latitude: 55.6713442, longitude: 12.523785)
+        ]
+              
+        XCTAssertEqual(locations.count, 3)
+        XCTAssertEqual(locations[0], expectedLocations[0])
+        XCTAssertEqual(locations[1], expectedLocations[1])
+        XCTAssertEqual(locations[2], expectedLocations[2])
+    }    
 }
 
 // MARK: - Helpers
 
 private final class HTTPClientSpy {
     
-    var stub: Error?
-    
+    var stub: Result<Data, Error> = .failure(NSError(domain: "", code: 1))
+        
     private(set) var capturedUrls: [URL] = []
     
-    func get(from url: URL) throws {
+    func get(from url: URL) throws -> Data {
         capturedUrls.append(url)
-        try stub.map { throw $0 }
+        return try stub.get()
     }
 }
