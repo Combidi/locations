b4101286bb350501b194c5b42286fd203ed9aa62
Peter Combee <petercombee@gmail.com>
2024-11-05 20:22:12 +0100

    `RemoteLocationsProvider.getLocations` gets locations from URL

---
+++ Locations/LocationsTests/RemoteLocationsProviderTests.swift
@@ -0,0 +1,46 @@
+//
+//  Created by Peter Combee on 05/11/2024.
+//
+
+@testable import Locations
+
+private struct RemoteLocationsProvider: LocationsProvider {
+    
+    private let httpClient: HTTPClientSpy
+    
+    init(httpClient: HTTPClientSpy) {
+        self.httpClient = httpClient
+    }
+    
+    func getLocations() async -> [Location] {
+        let locationsURL = URL(string: "https://raw.githubusercontent.com/abnamrocoesd/assignment-ios/main/locations.json")!
+        httpClient.get(from: locationsURL)
+        return []
+    }
+}
+
+import XCTest
+
+final class RemoteLocationsProviderTests: XCTestCase {
+    
+    func test() async {
+        let client = HTTPClientSpy()
+        let sut = RemoteLocationsProvider(httpClient: client)
+        
+        _ = await sut.getLocations()
+        
+        let expectedUrl = URL(string: "https://raw.githubusercontent.com/abnamrocoesd/assignment-ios/main/locations.json")!
+        XCTAssertEqual(client.capturedUrls, [expectedUrl])
+    }
+}
+
+// MARK: - Helpers
+
+private final class HTTPClientSpy {
+    
+    private(set) var capturedUrls: [URL] = []
+    
+    func get(from url: URL) {
+        capturedUrls.append(url)
+    }
+}
