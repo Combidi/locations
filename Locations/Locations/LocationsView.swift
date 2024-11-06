5c7d027dc9686fff4ef33e7704ea989b2884d388
Peter Combee <petercombee@gmail.com>
2024-11-06 08:33:02 +0100

    Add simple presenting view

--- Locations/Locations/LocationsView.swift
+++ Locations/Locations/LocationsView.swift
@@ -9,6 +9,7 @@ struct LocationsView: View {
     enum LoadingState {
         case loading
         case error
+        case presenting([Location])
     }
     
     let state: LoadingState
@@ -19,14 +20,26 @@ struct LocationsView: View {
             Text("Loading...")
         case .error:
             Text("Oeps! Something went wrong...")
+        case let .presenting(locations):
+            List(locations, id: \.name) { location in
+                Text(location.name)
+            }
         }
     }
 }
 
-#Preview {
+#Preview("Locations loading") {
     LocationsView(state: .loading)
 }
 
-#Preview {
+#Preview("Locations error") {
     LocationsView(state: .error)
+}
+
+#Preview("Locations presenting") {
+    LocationsView(state: .presenting([
+        Location(name: "Amsterdam", latitude: 0, longitude: 0),
+        Location(name: "Mumbai", latitude: 0, longitude: 0),
+        Location(name: "Copenhagen", latitude: 0, longitude: 0)
+    ]))
 }
