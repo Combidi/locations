fd8bf9164c44c2f1c210fe7a37676f13f919450f
Peter Combee <petercombee@gmail.com>
2024-11-06 20:59:05 +0100

    Move `LocationsPresentingView` to its own file

---
+++ Locations/Locations/View/LocationsPresentingView.swift
@@ -0,0 +1,43 @@
+//
+//  Created by Peter Combee on 06/11/2024.
+//
+
+import SwiftUI
+
+struct LocationsPresentingView: View {
+    
+    private let state: LocationsLoadingState
+    
+    init(state: LocationsLoadingState) {
+        self.state = state
+    }
+    
+    var body: some View {
+        switch state {
+        case .loading:
+            Text("Loading...")
+        case .error:
+            Text("Oeps! Something went wrong...")
+        case let .presenting(locations):
+            List(locations, id: \.name) { location in
+                Text(location.name)
+            }
+        }
+    }
+}
+
+#Preview("Locations loading") {
+    LocationsPresentingView(state: .loading)
+}
+
+#Preview("Locations error") {
+    LocationsPresentingView(state: .error)
+}
+
+#Preview("Locations presenting") {
+    LocationsPresentingView(state: .presenting([
+        Location(name: "Amsterdam", latitude: 0, longitude: 0),
+        Location(name: "Mumbai", latitude: 0, longitude: 0),
+        Location(name: "Copenhagen", latitude: 0, longitude: 0)
+    ]))
+}
