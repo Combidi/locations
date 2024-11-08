e72c154ed7b3a3ad975327f8bd4e2c5b07420c7b
Peter Combee <petercombee@gmail.com>
2024-11-08 20:46:25 +0100

    `WikipediaLocationNavigator. showLocationOnWikipedia` opens Wikipedia URL for location

---
+++ Locations/LocationsTests/WikipediaLocationNavigatorTests.swift
@@ -0,0 +1,69 @@
+//
+//  Created by Peter Combee on 08/11/2024.
+//
+
+import XCTest
+import Foundation
+@testable import Locations
+
+struct WikipediaLocationNavigator {
+    
+    private let openUrl: (URL) -> Void
+    
+    init(openUrl: @escaping (URL) -> Void) {
+        self.openUrl = openUrl
+    }
+    
+    func showLocationOnWikipedia(_ location: Location) {
+        let locationUrl = URL(string: "https://en.wikipedia.org")!
+            .appendingPathComponent("wiki")
+            .appendingPathExtension(location.name)
+        
+        openUrl(locationUrl)
+    }
+}
+
+final class WikipediaLocationNavigatorTests {
+    
+    func test_showLocationOnWikipedia_opensWikipediaUrl() {
+        let urlOpenerSpy = UrlOpenerSpy()
+        let sut = WikipediaLocationNavigator(openUrl: urlOpenerSpy.openUrl)
+                
+        XCTAssertEqual(
+            urlOpenerSpy.capturedUrls, [],
+            "Expected no URLs before show location"
+        )
+
+        let arnhem = Location(name: "Arnhem", latitude: 0, longitude: 0)
+        sut.showLocationOnWikipedia(arnhem)
+        
+        XCTAssertEqual(
+            urlOpenerSpy.capturedUrls,
+            [URL(string: "https://en.wikipedia.org/wiki/arnhem")!],
+            "Expected first URL"
+        )
+
+        let amsterdam = Location(name: "Arnhem", latitude: 0, longitude: 0)
+        sut.showLocationOnWikipedia(amsterdam)
+        
+        XCTAssertEqual(
+            urlOpenerSpy.capturedUrls,
+            [
+                URL(string: "https://en.wikipedia.org/wiki/arnhem")!,
+                URL(string: "https://en.wikipedia.org/wiki/amsterdam")!,
+            ],
+            "Expected second URL"
+        )
+    }
+}
+
+// MARK: - Helpers
+
+private final class UrlOpenerSpy {
+    
+    private(set) var capturedUrls: [URL] = []
+    
+    func openUrl(_ url: URL) {
+        capturedUrls.append(url)
+    }
+}
