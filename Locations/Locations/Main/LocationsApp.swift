e6f8c236eb8c06a5144553441d90e3435d9a9883
Peter Combee <petercombee@gmail.com>
2024-11-08 21:28:46 +0100

    Reduce nesting in composition

--- Locations/Locations/Main/LocationsApp.swift
+++ Locations/Locations/Main/LocationsApp.swift
@@ -4,21 +4,19 @@
 
 import SwiftUI
 
+let navigator = WikipediaLocationNavigator(openUrl: { UIApplication.shared.open($0) })
+let session = URLSession(configuration: .ephemeral)
+let httpClient = UrlSessionHttpClient(session: session)
+let locationProvider = RemoteLocationsProvider(httpClient: httpClient)
+
 @main
 struct LocationsApp: App {
     var body: some Scene {
         WindowGroup {
             LocationsView(
                 model: LocationsViewModel(
-                    locationsProvider: RemoteLocationsProvider(
-                        httpClient: UrlSessionHttpClient(
-                            session: URLSession(configuration: .ephemeral)
-                        )
-                    ),
-                    onLocationSelection: { location in
-                        WikipediaLocationNavigator(openUrl: { UIApplication.shared.open($0) })
-                        .showLocationOnWikipedia(location)
-                    }
+                    locationsProvider: locationProvider,
+                    onLocationSelection: navigator.showLocationOnWikipedia
                 )
             )
         }
