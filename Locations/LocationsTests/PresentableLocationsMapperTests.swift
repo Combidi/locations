f1789b0259668258a69b7a5cf845cd54669383ae
Peter Combee <petercombee@gmail.com>
2024-11-09 20:50:21 +0100

    `PresentableLocationMapper.mapTopPresentableLocations` notifies handler on location selection

--- Locations/LocationsTests/PresentableLocationsMapperTests.swift
+++ Locations/LocationsTests/PresentableLocationsMapperTests.swift
@@ -6,9 +6,19 @@ import XCTest
 @testable import Locations
 
 struct PresentableLocationMapper {
+    
+    private let onLocationSelection: (Location) -> Void
+    
+    init(onLocationSelection: @escaping (Location) -> Void) {
+        self.onLocationSelection = onLocationSelection
+    }
+    
     func mapToPresentableLocations(locations: [Location]) -> [PresentableLocation] {
         locations.map { location in
-            PresentableLocation(name: location.name, onSelection: {})
+            PresentableLocation(
+                name: location.name,
+                onSelection: { onLocationSelection(location) }
+            )
         }
     }
 }
@@ -16,7 +26,7 @@ struct PresentableLocationMapper {
 final class PresentableLocationsMapperTests: XCTestCase {
 
     func test_mapToPresentableLocations_correctlyMapsPresentableValues() {
-        let sut = PresentableLocationMapper()
+        let sut = PresentableLocationMapper(onLocationSelection: { _ in })
         
         let locations = [
             Location(name: "Amsterdam", latitude: 1, longitude: 2),
@@ -28,5 +38,45 @@ final class PresentableLocationsMapperTests: XCTestCase {
         XCTAssertEqual(presentables.count, 2)
         XCTAssertEqual(presentables[0].name, "Amsterdam")
         XCTAssertEqual(presentables[1].name, "Arnhem")
+    }
+    
+    func test_presentableLocationSelection_notifiesSelectionHandler() {
+        let selectionHandlerSpy = LocationSelectionHandlerSpy()
+        let sut = PresentableLocationMapper(onLocationSelection: selectionHandlerSpy.onLocationSelection)
+        
+        let amsterdam = Location(name: "Amsterdam", latitude: 1, longitude: 2)
+        let arnhem = Location(name: "Arnhem", latitude: 1, longitude: 2)
+        
+        let presentables = sut.mapToPresentableLocations(locations: [amsterdam, arnhem])
+ 
+        XCTAssertEqual(
+            selectionHandlerSpy.selectedLocations, [],
+            "Expected no locations after first location has been selected"
+        )
+        
+        presentables[1].onSelection()
+        
+        XCTAssertEqual(
+            selectionHandlerSpy.selectedLocations, [arnhem],
+            "Expected first location after Arnhem has been selected"
+        )
+
+        presentables[0].onSelection()
+
+        XCTAssertEqual(
+            selectionHandlerSpy.selectedLocations, [arnhem, amsterdam],
+            "Expected second location after Amsterdam has been selected"
+        )
+    }
+}
+
+// MARK: - Helpers
+
+private final class LocationSelectionHandlerSpy {
+    
+    private(set) var selectedLocations: [Location] = []
+    
+    func onLocationSelection(_ location: Location) {
+        selectedLocations.append(location)
     }
 }
